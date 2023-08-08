d0eb562a7c8fbdbb76e8e126a9a1d88934afbecf
Akleem khan <akleemkhan0208@gmail.com>
2023-08-08 19:25:16 +0530

    commit

---
+++ Pizza Mania/src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to="/"> Pizza Mani</Link>
+        </header>
+    )
+}
+
+export default Header
