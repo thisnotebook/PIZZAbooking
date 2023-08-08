5d6d44cb06d1769d933de9c1f1df9dcdc36e9b4a
Akleem khan <akleemkhan0208@gmail.com>
2023-08-08 20:16:14 +0530

    commit

--- Pizza Mania/src/features/menu/Menu.jsx
+++ Pizza Mania/src/features/menu/Menu.jsx
@@ -1,5 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData()
+  return (
+    <div>
+      <h1>Menu</h1>
+      {menu.map(pizza => <MenuItem pizza={pizza} key={pizza.id} />)}
+    </div>)
+
 }
 
 export default Menu;
